943f2c10c65f8961a770c447e7c73ab2418b96e2
Ben Richter <ben.k.richter@gmail.com>
2017-06-18 14:32:27 +0200

    Change name

--- build/config.js
+++ build/config.js
@@ -3,7 +3,7 @@ const pkg = require('../package')
 
 module.exports = {
   port: 4000,
-  title: 'steam-key-tumbler',
+  title: 'dishhh',
   // when you use electron please set to relative path like ./
   // otherwise only set to absolute path when you're using history mode
   publicPath: '/',
