b714e41556f741c98ec0276e49f05a27a7ca5511
Ben Richter <ben.k.richter@gmail.com>
2017-06-20 12:58:09 +0200

    Add htaccess for history mode

--- client/router/index.js
+++ client/router/index.js
@@ -6,11 +6,10 @@ import NotFound from '../views/NotFound.vue';
 
 Vue.use(Router);
 
-// todo: add catch all rule to htaccess https://router.vuejs.org/en/essentials/history-mode.html
 export default new Router({
     mode: 'history',
     routes: [
-        {path: '/', component: Home},
-        {path: '*', component: NotFound} // history mode disables server handling
+        { path: '/', component: Home },
+        { path: '*', component: NotFound } // history mode disables server handling
     ]
 });
