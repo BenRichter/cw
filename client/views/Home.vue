ad49511fa12553c9c5bcf1e8a141d89da22ef9ee
Ben Richter <ben.k.richter@gmail.com>
2017-06-21 16:40:41 +0200

    Remove unused containers

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -7,37 +7,24 @@
         </header>
 
         <div class="container">
-            <buttonCall></buttonCall>
-
             <alert-message></alert-message>
 
-            <key-input>Benson</key-input>
-
-            <hr/>
-
-            <key-output></key-output>
-
-            <counter unit="eggs"></counter>
-
-            <p>Share Link: <code>/share?id=123</code></p>
+            <buttonCallWaiter></buttonCallWaiter>
+            <buttonCallCheck></buttonCallCheck>
         </div>
     </div>
 </template>
 
 <script>
-    import counter from 'components/Counter';
-    import keyInput from 'components/KeyInput';
-    import keyOutput from '../components/KeyOutput';
     import alertMessage from '../components/AlertMessage.vue';
-    import buttonCall from '../components/ButtonCall.vue';
+    import buttonCallWaiter from '../components/ButtonCallWaiter.vue';
+    import buttonCallCheck from '../components/ButtonCallCheck.vue';
 
     export default {
         components: {
-            counter,
-            keyInput,
-            keyOutput,
             alertMessage,
-            buttonCall
+            buttonCallWaiter,
+            buttonCallCheck
         }
     };
 </script>
