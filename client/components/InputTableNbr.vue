3273c8a9f5ba5408d39b2f3981b6f3c295c516bb
Ben Richter <ben.k.richter@gmail.com>
2017-06-27 16:02:48 +0200

    Structure table form

--- client/components/InputTableNbr.vue
+++ client/components/InputTableNbr.vue
@@ -4,18 +4,34 @@
 
         <notification v-if="hasError" status="is-warning">Das ist keine gültige Tischnummer</notification>
 
-        <label for="tableNbr">Tischnummer</label>
-        <input type="number" min="1" step="1" v-model="tableNumber" id="tableNbr"/>
-        <button class="button is-primary" :class="{ 'is-loading' : isLoading }" @click="redirectTo">Ok!</button>
+        <div class="field is-horizontal has-addons">
+            <label for="tableNbr">Tischnummer</label>
+
+
+            <p class="control has-icons-left">
+                <!--todo: max number by restaurant-->
+                <input v-model="tableNumber" type="number" min="1" :max="tableCountMax" step="1" placeholder="&#xF002;" class="input is-large" id="tableNbr"/>
+                <span class="icon is-small is-left">
+                    <Icon name="user-circle" label="user-circle"></Icon>
+                </span>
+            </p>
+            <p class="control">
+                <button class="button is-primary is-large" :class="{ 'is-loading' : isLoading }" @click="redirectTo">Ok!</button>
+            </p>
+        </div>
     </div>
 </template>
 
 <script>
+    import 'vue-awesome/icons/user-circle';
+    import Icon from 'vue-awesome/components/Icon';
     import notification from './Notification.vue';
+
 
     export default {
         data() {
             return {
+                tableCountMax: 25,
                 tableNumber: '',
                 hasError: false,
                 isLoading: false
@@ -40,7 +56,8 @@
             }
         },
         components: {
-            notification
+            notification,
+            Icon
         }
     }
 </script>
