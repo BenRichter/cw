0c22db616787d4f4191e2e27de28db4f94ba0cdc
Ben Richter <ben.k.richter@gmail.com>
2017-06-27 15:18:41 +0200

    Add Landing PAge and table number Input

---
+++ client/components/InputTableNbr.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <p>An welchem Tisch sitzt du?</p>
+
+        <notification v-if="hasError" status="is-warning">Das ist keine gültige Tischnummer</notification>
+
+        <label for="tableNbr">Tischnummer</label>
+        <input type="number" min="1" step="1" v-model="tableNumber" id="tableNbr"/>
+        <button class="button is-primary" :class="{ 'is-loading' : isLoading }" @click="redirectTo">Ok!</button>
+    </div>
+</template>
+
+<script>
+    import notification from './Notification.vue';
+
+    export default {
+        data() {
+            return {
+                tableNumber: '',
+                hasError: false,
+                isLoading: false
+            }
+        },
+        methods: {
+            redirectTo() {
+                console.log('redirect to ' + this.tableNumber);
+                this.setLoading();
+                this.validateInput();
+                // push state to store!
+                // redirect to this.tableNumber  Home
+            },
+            validateInput() {
+                // todo: validate
+                console.log('validate!');
+
+                this.hasError = true;
+            },
+            setLoading() {
+                this.isLoading = true;
+            }
+        },
+        components: {
+            notification
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ client/views/Landing.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="section">
+        <div class="container">
+            <h1>Willkommen im xyz!</h1>
+
+            <input-tbl-nbr></input-tbl-nbr>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import inputTblNbr from '../components/InputTableNbr.vue';
+
+    export default {
+        components: {
+            inputTblNbr
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    /* Specific Page Styles */
+
+</style>
