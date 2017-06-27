1c650cdae9176d0fb1dbd86adf4d332e9785d05e
Ben Richter <ben.k.richter@gmail.com>
2017-06-27 15:19:02 +0200

    Add /table path to routing

--- client/router/index.js
+++ client/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from '../views/Home.vue';
+import Landing from '../views/Landing.vue';
 import NotFound from '../views/NotFound.vue';
 
 Vue.use(Router);
@@ -10,6 +11,7 @@ export default new Router({
     mode: 'history',
     routes: [
         { path: '/', component: Home },
+        { path: '/table', component: Landing },
         { path: '*', component: NotFound } // history mode disables server handling
     ]
 });
