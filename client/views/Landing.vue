7ee299c7750b2c78706a86a26a5ad7f93646823c
Ben Richter <ben.k.richter@gmail.com>
2017-08-02 16:25:16 +0200

    Fetch bar on mount

--- client/views/Landing.vue
+++ client/views/Landing.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="section">
         <div class="container">
+
             <div class="card">
                 <header class="card-header">
-                    <p class="card-header-title">Willkommen im xyz!</p>
+                    <p class="card-header-title">Willkommen im {{ bar }}</p>
                 </header>
                 <div class="card-content">
                     <div class="content">
@@ -11,17 +12,35 @@
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 </template>
 
 <script>
     import inputTblNbr from '../components/InputTableNbr.vue';
+//    import FetchData from 'vue-fetch-data';
 
     export default {
+        data: () => ({
+            isLoading: true,
+            isBar: false
+        }),
+        beforeMount() {
+            this.fetchBar();
+        },
+        props: ['bar'],
+        methods: {
+            fetchBar() {
+                console.log("fetch bar!!!");
+            }
+        },
         components: {
             inputTblNbr
         }
+
+        // router.replace(location, onComplete?, onAbort?)
+        // https://router.vuejs.org/en/essentials/navigation.html
     }
 </script>
 
