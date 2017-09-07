feb2f2220dff3ee5cea4a26c673d8bee6360c1e9
Ben Richter <ben.k.richter@gmail.com>
2017-09-07 19:43:02 +0200

    Basic validation Change table number icon to coffee, cause no table Icon

--- client/components/InputTableNbr.vue
+++ client/components/InputTableNbr.vue
@@ -2,13 +2,13 @@
     <div>
         <Notification v-if="hasError" status="is-warning">Das ist keine gültige Tischnummer</Notification>
 
-        <label for="tableNbr" class="label">An welchem Tisch sitzt du?</label>
+        <label for="tableNbr" class="label">Bitte gib deine Tischnummer an?</label>
         <div class="field has-addons">
             <p class="control has-icons-left">
                 <input v-model="tableNumber" type="number" min="1" :max="tableCountMax" step="1" :placeholder="tableCountPlaceholder" class="input is-large" id="tableNbr"/>
 
                 <span class="icon is-left">
-                    <Icon name="user-circle" label="User"></Icon>
+                    <Icon name="coffee" label="Table"></Icon>
                 </span>
             </p>
             <p class="control">
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-    import 'vue-awesome/icons/user-circle';
+    import 'vue-awesome/icons/coffee';
     import Icon from 'vue-awesome/components/Icon';
     import Notification from './Notification.vue';
 
@@ -27,7 +27,7 @@
         data() {
             return {
                 tableCountMax: 25, // todo: get number by restaurant
-                tableCountPlaceholder: '9\u00BE',
+                tableCountPlaceholder: '',
                 tableNumber: '',
                 hasError: false,
                 isLoading: false
@@ -35,20 +35,24 @@
         },
         methods: {
             redirectTo() {
-                console.log('redirect to ' + this.tableNumber);
-                this.setLoading();
-                this.validateInput();
-                // push state to store!
-                // redirect to this.tableNumber  Home
+                this.isLoading = true;
+
+
+                if (this.validateInput()) {
+                    console.log('valid');
+                    console.log('redirect to ' + this.tableNumber);
+                }
             },
             validateInput() {
-                // todo: validate
                 console.log('validate!');
-
-                this.hasError = true;
-            },
-            setLoading() {
-                this.isLoading = true;
+                if (this.tableNumber !== '' && this.tableNumber > 0 && this.tableNumber <= this.tableCountMax) {
+                    return true;
+                } else {
+                    // todo: specify error
+                    this.isLoading = false;
+                    this.hasError = true;
+                    return false;
+                }
             }
         },
         components: {
